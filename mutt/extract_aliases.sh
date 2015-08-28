08035f4464472232230f696cd4d5192fcdfe279a
Raj Sahae <rsahae@teslamotors.com>
2015-08-28 11:30:08 -0700

    Added script to extract aliases
    Also added aliases to repo

---
+++ mutt/extract_aliases.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+ALIASES=$HOME/.mutt/aliases
+MESSAGE=$(cat)
+
+NEWALIAS=$( \
+  echo "${MESSAGE}" | \
+  grep ^"From: " |    \
+  sed s/[\,\"\']//g | \
+  awk '{
+    $1="";
+    if (NF == 3)
+      {print "alias" $0;}
+    else if (NF == 2)
+      {print "alias" $0 $0;}
+    else if (NF > 3)
+      {print "alias", tolower($(NF-1))"-"tolower($2) $0;}
+  }'
+)
+
+if ! grep -Fxq "$NEWALIAS" $ALIASES; then
+  echo "$NEWALIAS" >> $ALIASES
+fi
+
+echo "${MESSAGE}"
