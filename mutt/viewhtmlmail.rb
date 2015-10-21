cfc339e5abaa55c4903b4ff0a0ba93e6449ff4e6
Raj Sahae <rsahae@teslamotors.com>
2015-10-21 11:34:14 -0700

    Aliases, gitconfig, viewhtmlmail.rb script changes
    Updated users for pull request aliases
    Updated git config with some diff helper aliases
    Updating alias file
    Tweaked viewhtmlmail.rb script to work better with multipart messages, I hope

--- mutt/viewhtmlmail.rb
+++ mutt/viewhtmlmail.rb
@@ -19,19 +19,62 @@
 # Add macros to your .muttrc file to use this, for example:
 #  macro  index  \Cv  ": unset pipe_decode\n<pipe-message>viewhtmlmail.rb\n: set pipe_decode\n" "View HTML in browser"
 #  macro  pager  \Cv  ": unset pipe_decode\n<pipe-message>viewhtmlmail.rb\n: set pipe_decode\n" "View HTML in browser"
- 
 
 require 'mail'
 require 'tmpdir'
+require 'pp'
+
+def extract_parts(ary, parent)
+  # collect all html messages. Dive into multipart/alternative to find html
+  parent.parts.inject(ary) do |memo, part|
+    if part.content_type =~ /text\/html/
+      memo.push(part)
+    elsif part.content_type =~ /multipart/
+      # puts '--- multiparts ---'
+      # pp part.parts
+      extract_parts(memo, part)
+    end
+    memo
+  end
+end
+
+def replace_images(mail, ary)
+  ary.each_with_index do |part, index|
+    # We need to replace the image src links with the local filename
+    # src="cid:image003.jpg@01D0D911.F135CA10" => image003.jpg
+    html = mail.attachments.inject(part.body.to_s) do |memo, attachment|
+
+      filename = attachment.filename
+      content_id = attachment.content_id[1...-1]
+
+      # p filename
+      # p content_id
+
+      if memo.include? attachment.filename
+        memo.gsub(/src="cid:#{filename}@[^"]*"/, "src=\"#{filename}\"")
+      elsif memo.include? attachment.content_id[1...-1]
+        memo.gsub(/src=.*#{content_id}"/, "src=\"#{filename}\"")
+      else
+        memo
+      end
+
+    end
+
+    File.open("mail-#{index}.html", 'w') do |file|
+      file.puts html
+    end
+
+  end
+end
 
 Dir.mktmpdir do |dir|
   Dir.chdir(dir) do 
 
-    str = STDIN.read
-    File.open('mail', 'w') { |file| file.puts str }
+    File.open('mail', 'w') { |file| file.puts STDIN.read }
     mail = Mail.read('mail')
 
-    # puts mail.attachments.map(&:inspect)
+    # puts "--- Attachments ---"
+    # pp mail.attachments
 
     # Shamelessly stolen right out of the mail gem README
     mail.attachments.each do |attachment|
@@ -45,49 +88,27 @@ Dir.mktmpdir do |dir|
       end
     end
 
+    # puts "--- mail.parts ---"
     # puts mail.parts.map(&:content_type).join("\n")
 
-    if mail.parts.empty?
+    parts = extract_parts([], mail)
+
+    # puts '--- extracted parts ---'
+    # pp parts
+
+    if parts.empty?
       puts 'mail parts empty'
       File.open("mail.html", 'w') do |file|
         file.puts mail.body.to_s
       end
     else
-
-      mail.parts.each_with_index do |part, index|
-        next unless part.content_type =~ /text\/html/
-
-        # puts mail.attachments.size
-
-        # We need to replace the image src links with the local filename
-        # src="cid:image003.jpg@01D0D911.F135CA10" => image003.jpg
-        html = mail.attachments.inject(part.body.to_s) do |memo, attachment|
-          # p memo
-
-          filename = attachment.filename
-          content_id = attachment.content_id[1...-1]
-
-          # p filename
-          # p content_id
-
-          if memo.include? attachment.filename
-            memo.gsub(/src="cid:#{filename}@[^"]*"/, "src=\"#{filename}\"")
-          elsif memo.include? attachment.content_id[1...-1]
-            memo.gsub(/src=.*#{content_id}"/, "src=\"#{filename}\"")
-          else
-            memo
-          end
-
-        end
-
-        File.open("mail-#{index}.html", 'w') do |file|
-          file.puts html
-        end
-      end
-
+      replace_images(mail, parts)
     end
 
     # Open all of the html pages we wrote out
+    # puts '--- html files ---'
+    # pp Dir['*html']
+
     Dir['*html'].each { |page| system("open #{page}") }
 
     # We need to wait a bit so the files are there when we try to open
